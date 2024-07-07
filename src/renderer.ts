eee932da0509f315f756bf5c5ab6e34ef62f1822
young-do <yd199323@gmail.com>
2024-07-07 12:50:44 +0900

    add react

--- src/renderer.ts
+++ src/renderer.ts
@@ -26,6 +26,9 @@
  * ```
  */
 
-import './index.css';
+import "./index.css";
+import "./app";
 
-console.log('👋 This message is being logged by "renderer.ts", included via Vite');
+console.log(
+  '👋 This message is being logged by "renderer.ts", included via Vite'
+);
