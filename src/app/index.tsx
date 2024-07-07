865ea58d03616ea47d3d46ecd0d07990787bcacc
young-do <yd199323@gmail.com>
2024-07-07 17:35:28 +0900

    app.tsx 파일 app 폴더로 이동

---
+++ src/app/index.tsx
@@ -0,0 +1,4 @@
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+ReactDOM.createRoot(document.querySelector('#app')!).render(<h2>Hello from React!</h2>);
--- src/renderer.ts
+++ src/renderer.ts
@@ -26,9 +26,4 @@
  * ```
  */
 
-import "./index.css";
-import "./app";
-
-console.log(
-  '👋 This message is being logged by "renderer.ts", included via Vite'
-);
+import './app';
