287086d16725e924925a7d4cbfbb6d962561c9c4
young-do <yd199323@gmail.com>
2024-07-10 00:22:04 +0900

    이미지 만드는 파이썬 스크립트

---
+++ script/README.md
@@ -0,0 +1,14 @@
+# split script 설명
+
+인터넷에 돌아다니는 gif를 tray icon으로 만들기위해 사용하는 script입니다.
+
+## 사용법
+
+1. pip install pillow
+2. python split.py [gif file path]
+3. 이후 생성된 파일들은 assets 폴더 내 gif 파일 이름의 폴더에 저장됩니다.
+
+## 이미지 저작권
+
+- 개발 학습 및 개인적인 용도로만 사용합니다.
+- 이미지 저작권은 원 저작자에게 있습니다.
---
+++ script/split.py
@@ -0,0 +1,40 @@
+from PIL import Image
+import os
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python script.py /path/to/gif/frames")
+    sys.exit(1)
+
+# GIF 파일 경로 설정
+gif_path = sys.argv[1]
+gif_dir = os.path.dirname(os.path.abspath(gif_path))
+gif_name = os.path.splitext(os.path.basename(gif_path))[0]
+output_dir = os.path.join(gif_dir, 'assets', gif_name)
+
+# 출력 디렉토리가 없으면 생성
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# GIF 파일 열기
+with Image.open(gif_path) as img:
+    frame = 0
+    while True:
+        try:
+            # 프레임을 22x22로 리사이즈
+            # width, height = img.size
+            new_height = 22
+            new_width = 22
+            img_resized = img.resize((new_width, new_height), Image.LANCZOS)
+            # img_resized2x = img.resize((new_width * 2, new_height * 2), Image.LANCZOS)
+
+            # PNG 파일로 저장
+            img_resized.save(os.path.join(output_dir, f'frame_{frame:03d}.png'))
+            # img_resized2x.save(os.path.join(output_dir, f'frame_{frame:03d}@2x.png'))
+            frame += 1
+            img.seek(frame)
+        except EOFError:
+            # 모든 프레임을 다 읽으면 종료
+            break
+
+print(f"All frames have been saved in {output_dir}")
