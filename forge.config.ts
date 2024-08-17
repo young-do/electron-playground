6c082ba119144d0fdc8fcfbd0804b7ecbf35a87f
young-do <yd199323@gmail.com>
2024-08-17 13:34:07 +0900

    release 테스트

--- forge.config.ts
+++ forge.config.ts
@@ -47,6 +47,17 @@ const config: ForgeConfig = {
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'young-do',
+          name: 'electron-playground',
+        },
+      },
+    },
+  ],
 };
 
 export default config;
